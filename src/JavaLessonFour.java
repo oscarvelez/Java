626501f4dd9588c25914fd5ece67d42d11f71478
Oscar <oscard_velez@hotmail.com>
2019-10-24 08:57:10 -0600

    lesson 4

---
+++ src/JavaLessonFour.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class JavaLessonFour {
+
+
+    public static void main(String[] args) {
+// for (declare iterator ; conditional statement ; change iterator)
+    for (int i = 10; i >= 1; i --){
+        System.out.println(i);
+    }
+
+    }
+}
