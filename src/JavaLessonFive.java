f06fc565eb07d961482fd9899942364ea8e3cf5e
Oscar <oscard_velez@hotmail.com>
2019-11-05 15:48:20 -0700

    lesson 4

---
+++ src/JavaLessonFive.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class JavaLessonFive {
+
+    static double myPi = 3.14159; //Class Variable
+
+    public static void main(String[] args){
+
+        System.out.println( addThem(1,2));
+
+    }
+
+    public static int addThem(int a, int b){
+
+        int c = a + b ;
+
+        return c;
+
+    }
+
+}
